7741e8cf187e6e12150c0263c469a759d2013bdf
cyberyugun <yudigunawan52@gmail.com>
2019-12-19 06:43:15 +0700

    Login Register ForgotPassword

--- Route/account.js
+++ Route/account.js
@@ -1,6 +1,5 @@
 const router                = require('express').Router();
 const accountController     = require('../Controller').user;
-router.get('/',accountController.getProfile);
 router.post('/signup', accountController.signup);
 router.post('/forgot-password', accountController.forgotPassword);
 
