a3d7396b6b14912247459dd9cea6ffaa97d97c48
cyberyugun <yudigunawan52@gmail.com>
2020-01-14 14:56:48 +0700

    Completed

--- Config/Mysql/mysql.js
+++ Config/Mysql/mysql.js
@@ -7,7 +7,7 @@ const Sequelize = require('sequelize');
 
 
 // online
-const sequelize = new Sequelize('RJcWxuKtvm', 'RJcWxuKtvm', 'sjSuQVV4E2', {
+const sequelize = new Sequelize('RJcWxuKtvm', 'RJcWxuKtvm', 'NBtSjLSUX5', {
     dialect: 'mysql',
     host: 'remotemysql.com'
 });
